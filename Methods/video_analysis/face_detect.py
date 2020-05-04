567c4eb8d44aa8adbb8be61f712cb5c32357fedd
pavithranp <ptpn94@gmail.com>
2020-05-05 01:05:01 +0200

    simple haarcascade face

--- Methods/video_analysis/face_detect.py
+++ Methods/video_analysis/face_detect.py
@@ -6,31 +6,57 @@ import imutils
 import time
 import mtcnn
 import cv2
+import os
 import tensorflow as tf
 from tensorflow.python.framework import ops
+from PIL import Image
 
 
 class FaceExtract:
+    cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
+    haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(haar_model)
+    print(haar_model)
+    face_cascade.load(haar_model)
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
     # construct the argument parse and parse the arguments
     def argument_parser(self):
         args = argparse.ArgumentParser()
         args.add_argument("-v", "--video", required=True,
                           help="path to input video file")
+        args.add_argument("--output-dir",
+                          help="path to output files")
+        args.add_argument("--detect", default="haarcascade", help="choose the model to detect faces")
         return args.parse_args()
+
+    def crop(self, image_obj, shape, saved_location):
+        x, y, w, h = shape
+        box = (y, y + h, x, x + w)
+        print(box)
+        print(sum(n < 0 for n in box))
+        if sum(n < 0 for n in box) == 0:
+            cv2.imwrite(saved_location, image_obj[box[0]:box[1], box[2]:box[3]])
+        else:
+            cv2.imwrite(saved_location, image_obj)
 
     def read_frames(self):
         # start the file video stream thread and allow the buffer to
         # start to fill
         print("[INFO] starting video file thread...")
         args = vars(self.argument_parser())
+        print(args)
         fvs = FileVideoStream(args["video"]).start()
         time.sleep(1.0)
 
         # start the FPS timer
         fps = FPS().start()
         detector = mtcnn.MTCNN()
+        fno = 0
         # loop over frames from the video file stream
         while fvs.more():
+            fno += 1
+            print(fno)
             # grab the frame from the threaded video file stream, resize
             # it, and convert it to grayscale (while still retaining 3
             # channels)
@@ -40,9 +66,15 @@ class FaceExtract:
             except:
                 break
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frame = np.dstack([frame, frame, frame])
-            results = detector.detect_faces(frame)
-            print(results)
+            # frame = np.dstack([frame, frame, frame])
+
+            if args["detect"] == "haarcascade":
+                results = self.face_cascade.detectMultiScale(frame, 1.3, 5)
+                print(results)
+                self.crop(frame, results[0], str(fno) + ".jpg")
+            else:
+                results = detector.detect_faces(frame)
+
             # display the size of the queue on the frame
             # cv2.putText(frame, "Queue Size: {}".format(fvs.Q.qsize()),
             #            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
